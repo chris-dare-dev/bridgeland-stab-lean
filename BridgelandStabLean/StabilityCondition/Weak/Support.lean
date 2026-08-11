2528966f4496b6489a27abfd0306bc71096e8480
Chris Dare <chris.dare@nalej.com>
2026-08-11 16:23:23 -0400

    Add uniform quadratic support infrastructure

--- BridgelandStabLean/StabilityCondition/Weak/Support.lean
+++ BridgelandStabLean/StabilityCondition/Weak/Support.lean
@@ -2,6 +2,6 @@
 Copyright (c) 2026 Chris Dare. All rights reserved.
 Released under the MIT license.
 -/
-import BridgelandStabLean.StabilityCondition.Weak.Support.Basic
+import BridgelandStabLean.StabilityCondition.Weak.Support.Quadratic
 
 /-! # Support for weak stability -/
